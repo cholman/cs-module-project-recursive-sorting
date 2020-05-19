137e67ae499b9b168b8d7edf2931ad6211466757
Cody Holman <holman.cody@gmail.com>
2020-05-19 17:43:54 -0500

    MVP

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,9 +2,25 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
+    
+    a = 0
+    b = 0
     # Your code here
-
+    # put back together here
+    # sort happens here
+    for k in range(0, elements):
+        if a >= len(arrA):
+            merged_arr[k] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[k] = arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[k] = arrA[a]
+            a += 1
+        else:
+            merged_arr[k] = arrB[b]
+            b += 1
 
     return merged_arr
 
@@ -12,7 +28,16 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
+    #split here
+    if len(arr) > 1:
 
+        #find the middle of arr
+        #sort stuff in left and put stuff to the left  in left
+        left = merge_sort( arr[0 : len(arr) // 2])
+        #sort stuff in right and put stuff to the right in right
+        right = merge_sort( arr[len(arr) // 2 :])
+        #merge left and right
+        arr = merge(left, right)
 
     return arr
 
@@ -20,6 +45,27 @@ def merge_sort(arr):
 # implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    start2 = mid + 1
+
+    if (arr[mid] <= arr[start2]):
+        return
+    
+    while (start <= mid and start2 <= end):
+        if (arr[start] <= arr[start2]):
+            start += 1
+        else: 
+            value = arr[start2]
+            index = start2
+        
+            while (index != start):
+                arr[index] = arr[index - 1]
+                index -= 1
+
+            arr[start] = value
+
+            start += 1
+            mid += 1
+            start2 += 1
 
 
     return arr
@@ -27,7 +73,13 @@ def merge_in_place(arr, start, mid, end):
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    if (l < r):
+        m = l + (r - l) // 2
 
+        merge_sort_in_place(arr, l, m)
+        merge_sort_in_place(arr, m + 1, r)
+
+        merge_in_place(arr, l, m, r)
 
     return arr
 
